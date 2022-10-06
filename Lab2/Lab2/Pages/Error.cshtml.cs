5762a9784b4550284588072b14cb4e87b573e913
Mohand Kessi <mohandkessi808@gmail.com>
2022-10-05 20:59:36 -0400

    Initial commit

--- Lab2/Lab2/Pages/Error.cshtml.cs
+++ Lab2/Lab2/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace WebMailKit.Pages
+namespace Lab2.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
